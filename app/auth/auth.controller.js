679bb950a264f30142434257019c4eb0286da51a
Artem Pavlenko <artempavlenko1703@gmail.com>
2023-01-30 19:39:43 +0300

    Add express-async-handler in project

--- app/auth/auth.controller.js
+++ app/auth/auth.controller.js
@@ -1,8 +1,10 @@
+import asyncHandler from 'express-async-handler';
+
 // @desk    Auth user
 // @route   POST /api/auth/login
 // @access  Public
-export const authUser = async (req, res) => {
+export const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
   res.send({ email });
-};
+});
